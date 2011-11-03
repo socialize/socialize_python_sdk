a61f423850163cd1182b718eba9489cd4910e34b
Peerakit Champ Somsuk <beyourwish@gmail.com>
2011-11-03 16:06:08 -0700

    create coverage report

--- build.sh
+++ build.sh
@@ -8,57 +8,33 @@ cd  `dirname $0`
 
 OS="`uname`"
 INSTALL_DIR=${INSTALL_DIR:=_install} 
-CACHE_DIR=${CACHE_DIR:=_pip_cache}
-
 
 build() {
+    echo ----------- Building virtual environment ----------------
     VPYTHON=python2.7
     [ "$OS" == "Darwin" ] && VPYTHON=python
     virtualenv --no-site-packages --distribute $INSTALL_DIR
+    echo ----------- INSTALL REQUIREMENTS ----------------
     $INSTALL_DIR/bin/pip install -E $INSTALL_DIR -r requirements.pip
 }
 
 clean() {
+    echo ----------- Clean up -----------------
     rm -rf $INSTALL_DIR
     rm -rf \.coverage
+    rm -rf html coverage.xml
     find . -iname "*.pyc" | xargs rm -rvf
 }
 
-
-unit_test() {
-    echo ========== Building API ==========
+unit_test(){
+    
     clean
     build
-    echo ========== Removing old files ==========
-    pushd ssz/ssz
-    rm -rvf tests/ \.coverage
-    popd
-    echo ========== Setting up path ==========
-    OLDPATH=$PATH
-    export PATH=$PATH:_install/bin
-    pushd _install/bin
-    echo ========== Activating virtualenv ==========
-    source activate
-    COVERAGE=`which coverage`
-    ./python $COVERAGE erase
-    echo ========== Running tests ==========
-    ./python $COVERAGE -x ./manage.py test --verbosity 0 xml-
-    ./python $COVERAGE html -d tests/html;
-    echo ========== Updating documentation ==========
-    pushd ../../ssz/ssz
-    ../../_manage.py update_documentation
-    popd
-    echo ========== Deactivating virtualenv ==========
-    deactivate
-    popd
-    export PATH=$OLDPATH
-    echo ========== Pushing to dev.api ==========
-    pushd deploy/fabric
-    fab deploy_dev
-    popd
-    echo ========== Done ==========
+    echo ----------- Running Unit Test ----------------
+    
+    _install/bin/nosetests tests --with-coverage
+    _install/bin/coverage xml
 }
-
 
 usage() {
     echo "$0 [build|clean|unit_test]"
