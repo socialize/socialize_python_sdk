86d5dc54e9e802c318497c17503f7d70f7c3003c
Sean Shadmand <sean.shadmand@gmail.com>
2012-01-20 16:42:50 -0800

    fixing up send notfiication endpoint

--- socialize/base.py
+++ socialize/base.py
@@ -23,7 +23,7 @@ class PartnerBase(object):
             }                    
 
     partner_endpoint_verb = {
-            'application' : ['upload_p12','upload_icon'],
+            'application' : ['upload_p12','upload_icon', 'notification'],
             'apiuser' : ['ban','unban','banned']
             }
 
@@ -99,6 +99,7 @@ class ObjectBase(PartnerBase):
             else:
                 raise Exception('%s is not allow in %s endpoint'%(verb, endpoint))
         request = Request(self.key,self.secret)
+        logger.info(request_url)
         return request.post(request_url, payload)   
 
     def _put(self, endpoint, payload, item=None, verb=None):
