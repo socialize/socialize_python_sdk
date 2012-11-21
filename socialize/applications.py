357f7a1db3db8dfdefb4a4bb1a12d7dd1031077c
Sean Shadmand <sean.shadmand@gmail.com>
2012-11-21 14:38:07 -0800

    adding twitter secret key as well

--- socialize/applications.py
+++ socialize/applications.py
@@ -139,6 +139,7 @@ class Application(ObjectBase):
             self.c2dm_token_source          =app.get('c2dm_token_source', 'socialize')
             
             self.twitter_consumer_key       =app.get('twitter_consumer_key', None)
+            self.twitter_consumer_secret       =app.get('twitter_consumer_secret', None)
             
             self.notification_quotas        =app.get('quotas', {})
             self.__update_notification_quotas()
